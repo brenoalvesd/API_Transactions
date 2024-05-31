f28c238aeb9b3d619a684cf2327c4d59841e295a
Breno Alves <breno7572@gmail.com>
2024-05-31 16:14:29 -0300

    Criando Rota para registrar uma transação

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-  .insert({
-    id: crypto.randomUUID(),
-    title: 'Transação teste',
-    amount: 1000,
-  })
-  .returning('*')
-  
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
