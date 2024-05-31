a6871831a4f7f9622e15261b131f92a9a44cd23c
Breno Alves <breno7572@gmail.com>
2024-05-31 13:24:58 -0300

    Criando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transaction = await knex('transactions')
+  .insert({
+    id: crypto.randomUUID(),
+    title: 'Transação teste',
+    amount: 1000,
+  })
+  .returning('*')
+  
+  return transaction
 })
 
 app
 .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server is Running')
