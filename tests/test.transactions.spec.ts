bd08b10f17836a398c264475bdcceb1f8eb4a431
Breno Alves <breno7572@gmail.com>
2024-06-03 17:12:54 -0300

    Criando teste e2e para listagem de transações

--- tests/test.transactions.spec.ts
+++ tests/test.transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -12,7 +12,7 @@ describe('Transactions routes', () => {
         await app.close()
     })
 
-    test('User can create a new transaction', async () => {
+    test('User should be able to create a new transaction', async () => {
         await request(app.server)
             .post('/transactions')
             .send({
@@ -21,5 +21,29 @@ describe('Transactions routes', () => {
                 type: 'credit'
             })
             .expect(201)
+    })
+
+    test('User should be able to list your transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'Teste',
+                amount: 100,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'Teste',
+                amount: 100,
+            })
+        ])
     })
 })
