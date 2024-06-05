3ddd114a9cc0f369ce2eba58e31af4c718098364
Breno Alves <breno7572@gmail.com>
2024-06-05 12:02:59 -0300

    Criando Teste para listar uma transação específica pelo seu ID

--- tests/test.transactions.spec.ts
+++ tests/test.transactions.spec.ts
@@ -52,4 +52,34 @@ describe('Transactions routes', () => {
             })
         ])
     })
+    test('User should be able to get a specific transaction', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'Teste',
+                amount: 100,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        const transactionId = listTransactionsResponse.body.transactions[0].id
+
+        const getTransactionResponse = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'Teste',
+                amount: 100,
+            })
+        )
+    })
 })
